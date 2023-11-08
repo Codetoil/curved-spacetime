aeca785a0265cac3d8daf76f5a234018e823f02d
Anthony Michalek (Codetoil) <ianthisawesomee@gamil.com>
2023-11-07 22:21:58 -0500

    Add License

--- vulkan-glfw-render-module/src/main/java/io/github/codetoil/curved_spacetime/render/vulkan/VulkanGraphicsQueue.java
+++ vulkan-glfw-render-module/src/main/java/io/github/codetoil/curved_spacetime/render/vulkan/VulkanGraphicsQueue.java
@@ -1,3 +1,23 @@
+/**
+ * Curved Spacetime is an easy-to-use modular simulator for General Relativity.<br>
+ * Copyright (C) 2023  Anthony Michalek (Codetoil)<br>
+ * <br>
+ * This file is part of Curved Spacetime<br>
+ * <br>
+ * This program is free software: you can redistribute it and/or modify <br>
+ * it under the terms of the GNU General Public License as published by <br>
+ * the Free Software Foundation, either version 3 of the License, or <br>
+ * (at your option) any later version.<br>
+ * <br>
+ * This program is distributed in the hope that it will be useful,<br>
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of<br>
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the<br>
+ * GNU General Public License for more details.<br>
+ * <br>
+ * You should have received a copy of the GNU General Public License<br>
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.<br>
+ */
+
 package io.github.codetoil.curved_spacetime.render.vulkan;
 
 import io.github.codetoil.curved_spacetime.vulkan.VulkanLogicalDevice;
