c1c309ddbf58b1da970a156024bd238d10e464af
Anthony Michalek <ianthisawesomee@gmail.com>
2025-04-29 13:06:22 -0400

    Fix bug

--- curved-spacetime-installer/install.bat
+++ curved-spacetime-installer/install.bat
@@ -1,8 +1,8 @@
 @echo off
 cmd /c ""apache-maven-3.9.9/bin/mvn.cmd" -U install"
-del target/curved-spacetime-installer-0.1.0-SNAPSHOT.jar
-del maven-archiver/pom.properties
-rmdir maven-archiver
+del target\curved-spacetime-installer-0.1.0-SNAPSHOT.jar
+del target\maven-archiver\pom.properties
+rmdir target\maven-archiver
 rmdir target
 echo Please press ENTER to exit.
 pause >nul
