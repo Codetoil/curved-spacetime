9bfd2430b277d45db70dd1e4bd49446e70984a03
Anthony Michalek <ianthisawesomee@gmail.com>
2025-04-29 12:30:28 -0400

    Maven updates!

--- curved-spacetime-installer/install.bat
+++ curved-spacetime-installer/install.bat
@@ -1,4 +1,4 @@
 @echo off
-cmd /c ""apache-maven-3.9.9/bin/mvn.cmd" -q install"
+cmd /c ""apache-maven-3.9.9/bin/mvn.cmd" -q -U install"
 echo Please press ENTER to exit.
 pause >nul
--- curved-spacetime-installer/install.sh
+++ curved-spacetime-installer/install.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-./apache-maven-3.9.9/bin/mvn -q install
+./apache-maven-3.9.9/bin/mvn -q -U install
 echo "Please press ENTER to exit."
 read dummyVar
