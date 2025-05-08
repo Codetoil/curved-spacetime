d0b8c75ed61866da125a7de7219babf842909f8e
Anthony Michalek <ianthisawesomee@gmail.com>
2025-05-07 22:02:00 -0400

    Update install.sh

--- curved-spacetime-installer/install.sh
+++ curved-spacetime-installer/install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 ./apache-maven-3.9.9/bin/mvn -U install
-rm target/curved-spacetime-installer-0.1.0-SNAPSHOT.jar target/maven-archiver/pom.properties
+rm target/curved-spacetime-installer-pomxml-0.1.0-SNAPSHOT.jar target/maven-archiver/pom.properties
 rmdir target/maven-archiver target
 echo "Please press ENTER to exit."
 read dummyVar
