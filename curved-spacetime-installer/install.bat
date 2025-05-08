aaa281bf24fc14589d342bdecd9824f2f1bd0a27
Anthony Michalek <ianthisawesomee@gmail.com>
2025-05-07 22:32:37 -0400

    Update installers

--- curved-spacetime-installer/install.bat
+++ curved-spacetime-installer/install.bat
@@ -1,8 +1,9 @@
 @echo off
-cmd /c ""apache-maven-3.9.9/bin/mvn.cmd" -U install"
+cmd /c ""apache-maven-3.9.9\bin\mvn.cmd" -q -Dmaven.repo.local=%userprofile%\.curved-spacetime\maven\ -U install"
+attrib +h %userprofile%\curved-spacetime
 del target\curved-spacetime-installer-pomxml-0.1.0-SNAPSHOT.jar
 del target\maven-archiver\pom.properties
 rmdir target\maven-archiver
 rmdir target
-echo Please press ENTER to exit.
+echo Done. Press any key (Such as ENTER) to exit the installer.
 pause >nul
--- curved-spacetime-installer/install.sh
+++ curved-spacetime-installer/install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-./apache-maven-3.9.9/bin/mvn -U install
+./apache-maven-3.9.9/bin/mvn -q -Dmaven.repo.local=$HOME/.curved-spacetime/maven/ -U install
 rm target/curved-spacetime-installer-pomxml-0.1.0-SNAPSHOT.jar target/maven-archiver/pom.properties
 rmdir target/maven-archiver target
-echo "Please press ENTER to exit."
+echo "Done. Press ENTER to exit the installer."
 read dummyVar
