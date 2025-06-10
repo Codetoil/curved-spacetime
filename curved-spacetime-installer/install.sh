cc6ea3987a23e6747c7af9ffbc4233e2148e30a7
Anthony Michalek <ianthisawesomee@gmail.com>
2025-06-09 22:05:38 -0400

    Update maven and packages.

--- curved-spacetime-installer/install.sh
+++ curved-spacetime-installer/install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-./apache-maven-3.9.9/bin/mvn -q -Dmaven.repo.local=$HOME/.curved-spacetime/maven/ -U install
+./apache-maven-3.9.10/bin/mvn -q -Dmaven.repo.local=$HOME/.curved-spacetime/maven/ -U install
 rm target/curved-spacetime-installer-pomxml-0.1.0-SNAPSHOT.jar target/maven-archiver/pom.properties
 rmdir target/maven-archiver target
 echo "Done. Press ENTER to exit the installer."
