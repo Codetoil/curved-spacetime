c577fb401d4aa64492ef1b2cae791736a06c3b44
Angelina Michalek <ianthisawesomee@gmail.com>
2025-09-21 12:51:45 -0400

    Minor fix

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,16 +13,16 @@ include("curved-spacetime-webserver-openapi-module")
 include("curved-spacetime-main-module")
 include("curved-spacetime-loader-module")
 include("curved-spacetime-quilt-loader-patches")
-/*project(":curved-spacetime-quilt-tweaker-agent").name = "curved-spacetime-quilt-tweaker-agent"
-project(":curved-spacetime-glfw-render-module").name = "curved-spacetime-glfw-render-module"
-project(":curved-spacetime-vulkan-module").name = "curved-spacetime-vulkan-module"
-project(":curved-spacetime-glfw-module").name = "curved-spacetime-glfw-module"
-project(":curved-spacetime-vulkan-glfw-module").name = "curved-spacetime-vulkan-glfw-module"
-project(":curved-spacetime-vulkan-glfw-render-module").name = "curved-spacetime-vulkan-glfw-render-module"
-project(":curved-spacetime-vulkan-render-module").name = "curved-spacetime-vulkan-render-module"
-project(":curved-spacetime-render-module").name = "curved-spacetime-render-module"
+project(":curved-spacetime-quilt-tweaker-agent").name = "curved-spacetime-quilt-tweaker-agent"
+//project(":curved-spacetime-glfw-render-module").name = "curved-spacetime-glfw-render-module"
+//project(":curved-spacetime-vulkan-module").name = "curved-spacetime-vulkan-module"
+//project(":curved-spacetime-glfw-module").name = "curved-spacetime-glfw-module"
+//project(":curved-spacetime-vulkan-glfw-module").name = "curved-spacetime-vulkan-glfw-module"
+//project(":curved-spacetime-vulkan-glfw-render-module").name = "curved-spacetime-vulkan-glfw-render-module"
+//project(":curved-spacetime-vulkan-render-module").name = "curved-spacetime-vulkan-render-module"
+//project(":curved-spacetime-render-module").name = "curved-spacetime-render-module"
 project(":curved-spacetime-webserver-module").name = "curved-spacetime-webserver-module"
 project(":curved-spacetime-webserver-openapi-module").name = "curved-spacetime-webserver-openapi-module"
 project(":curved-spacetime-main-module").name = "curved-spacetime-main-module"
 project(":curved-spacetime-loader-module").name = "curved-spacetime-loader-module"
-project(":curved-spacetime-quilt-loader-patches").name = "curved-spacetime-quilt-loader-patches"*/
+project(":curved-spacetime-quilt-loader-patches").name = "curved-spacetime-quilt-loader-patches"
