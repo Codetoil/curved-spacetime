0b1f8b8d88eef75eb9704a541fae044879ac8344
Angelina Michalek <ianthisawesomee@gmail.com>
2026-01-16 05:08:01 -0500

    Dependency Bumps

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,16 +8,16 @@ plugins {
 
 // Maven Central - https://repo1.maven.org/maven2/
 project.extra["lwjglVersion"] = "3.3.6" // org.lwjgl:lwjgl, org.lwjgl:lwjgl-glfw, org.lwjgl:lwjgl-vulkan
-project.extra["junitVersion"] = "6.0.1" // org.junit:junit-bom
+project.extra["junitVersion"] = "6.0.2" // org.junit:junit-bom
 project.extra["tinyLoggerVersion"] = "2.8.0-M1" // org.tinylog:tinylog-impl
 project.extra["gsonVersion"] = "2.13.2" // com.google.code.gson:gson
 project.extra["guavaVersion"] = "33.5.0-jre" // com.google.guava:guava
 
 // FabricMC Maven - https://maven.fabricmc.net/
-project.extra["fabricMixinVersion"] = "0.16.5+mixin.0.8.7" // net.fabricmc:sponge-mixin
+project.extra["fabricMixinVersion"] = "0.17.0+mixin.0.8.7" // net.fabricmc:sponge-mixin
 
 // QuiltMC Maven - https://maven.quiltmc.org/repository/release/
-project.extra["quiltLoaderVersion"] = "0.29.3-beta.1" // org.quiltmc:quilt-loader, org.quiltmc:quilt-loader-dependencies
+project.extra["quiltLoaderVersion"] = "0.30.0-beta.0" // org.quiltmc:quilt-loader, org.quiltmc:quilt-loader-dependencies
 
 project.extra["lwjglNativesNames"] = listOf(
     "natives-linux",
